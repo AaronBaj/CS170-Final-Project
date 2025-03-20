70ed42d44396ad84457a1d48eabf683b632db1b1
AaronBaj <141007966+AaronBaj@users.noreply.github.com>
2025-03-20 13:02:38 -0700

    Update Final Proj CS170.py

--- Final Proj CS170.py
+++ Final Proj CS170.py
@@ -1,27 +1,33 @@
 import numpy as np
 
 def leave_one_out_cross_validation(data, current_set, feature_to_add):
-    return 1
+    new_data = np.copy(data)
 
-def cs170demo():
-    data = np.loadtxt("CS170_Small_Data__96.txt")
-    
-    for i in range(len(data)):
-        object_to_classify = data[i, 1:]
-        label_object_to_classify = data[i,0]
+    for i in range(len(new_data)):
+        for j in range(1, len(new_data[0])):
+            if j not in current_set and j != feature_to_add:
+                new_data[i, j] = 0
+
+    number_correctly_classified = 0
+    for i in range(len(new_data)):
+        object_to_classify = new_data[i, 1:]
+        label_object_to_classify = new_data[i,0]
         nearest_neighbor_distance = float('inf')
         nearest_neighbor_location = float('inf')
 
-        for k in range(len(data)):
+        for k in range(len(new_data)):
             if k != i:
-                distance = np.sqrt(np.sum((object_to_classify - data[k, 1:]) ** 2))
+                distance = np.sqrt(np.sum((object_to_classify - new_data[k, 1:]) ** 2))
 
                 if distance < nearest_neighbor_distance:
                     nearest_neighbor_distance = distance
                     nearest_neighbor_location = k
-                    nearest_neighbor_label = data[nearest_neighbor_location, 0]
-        print(f"Object {i} is class {label_object_to_classify}")
-        print(f"Its nearest_neighbor is {nearest_neighbor_location} which is in class {nearest_neighbor_label}")
+                    nearest_neighbor_label = new_data[nearest_neighbor_location, 0]
+        if label_object_to_classify == nearest_neighbor_label:
+            number_correctly_classified = number_correctly_classified + 1
+    accuracy = number_correctly_classified / len(new_data)
+    return accuracy
+    
 
 def feature_search_demo(data):
     current_set_of_features = []
@@ -33,7 +39,7 @@ def feature_search_demo(data):
         for k in range(1, len(data[0])):
             if k not in current_set_of_features:
                 print(f"--Consider adding the {k} feature")
-                accuracy = leave_one_out_cross_validation(data, current_set_of_features, k + 1)
+                accuracy = leave_one_out_cross_validation(data, current_set_of_features, k)
 
                 if accuracy > best_so_far_accuracy:
                     best_so_far_accuracy = accuracy
@@ -43,5 +49,4 @@ def feature_search_demo(data):
 
 data = np.loadtxt("CS170_Small_Data__96.txt")
 
-#feature_search_demo(data)
-cs170demo()
+feature_search_demo(data)
